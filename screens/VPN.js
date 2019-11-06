e3bd4fd89e3751489ddeb105b03a1ffb15bc231b
Deng Wuor Joak <dengjoak@gmail.com>
2019-11-06 13:41:35 +0100

    ,

---
+++ screens/VPN.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export default class VPN extends Component {
+  render() {
+    return (
+      <View>
+        <Text> VPN </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Welcome </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
